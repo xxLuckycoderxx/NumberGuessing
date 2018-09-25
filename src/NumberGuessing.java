172e90bdb3b3060980e7e5cd47242f0088bdc0c4
Student675 <moses.tibambuya.2022@student.lhschools.org>
2018-09-25 08:14:36 -0400

    changes were made

---
+++ src/NumberGuessing.java
@@ -0,0 +1,16 @@
+import static java.lang.Math.random;
+
+public class NumberGuessing {
+    public static void main(String[]args){
+
+
+     int chosenNumber;
+     chosenNumber= (int) Math.random();
+
+     System.out.println();
+
+
+
+
+    }
+}
