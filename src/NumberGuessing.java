90f8fcc3689fda04ef7d81d05e78b5d5d42b8f0d
Student675 <moses.tibambuya.2022@student.lhschools.org>
2018-09-27 08:05:31 -0400

    more changes in my code

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
 import static java.lang.Math.random;
 
 public class NumberGuessing {
     public static void main(String[]args){
 
-
+     Scanner keyboard;
+     int input;
      int chosenNumber;
-     chosenNumber= (int) Math.random();
+     chosenNumber= (int)(Math.random()* 100);
+     keyboard=new Scanner(System.in);
 
-     System.out.println();
 
+     System.out.println("Enter a random number !");
+     input=keyboard.nextInt();
 
+     if(input==chosenNumber){
+     System.out.println("Great Job !");
+   }
 
 
     }
