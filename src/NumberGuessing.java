19cf06073eb63bd228e9eda689da399002571e40
Student675 <moses.tibambuya.2022@student.lhschools.org>
2018-10-18 07:28:02 -0400

    changes were made

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -27,5 +27,36 @@ public class NumberGuessing {
      }
      else
      System.out.print("Try Again!");
-    }
+
+
+
+
+
+
+     Scanner keyboard;
+     keyboard=new Scanner(System.in);
+     boolean mag = true;
+     int numb;
+     int dose=(int)(Math.random()*100);
+
+
+     System.out.println("Please enter a value from 1-100");
+     numb=keyboard.nextInt();
+     do {
+      if( numb >dose){
+       System.out.println("Too hot");
+      } mag=false;
+      elseif(numb<dose){
+       System.out.println("Tooo cold");
+      }
+            else if(numb==dose){
+       System.out.println("Bingo");
+      }break;
+
+
+
+
+
+
+     }
 }
